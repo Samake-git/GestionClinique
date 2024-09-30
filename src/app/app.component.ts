996c1d7477dd6102a0c5006adc5fb0c15cdcf5cd
Samake-git <samakebakary036@gmail.com>
2024-09-30 17:08:53 +0000

    ajout service authentification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,87 @@
-import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { NavigationEnd, RouterModule, RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatDividerModule } from '@angular/material/divider';
+import { NgIf } from '@angular/common';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, LoginComponent, MatIconModule, MatSidenavModule, MatToolbarModule,
+    MatButtonModule,
+    MatListModule,
+    RouterModule,
+    MatSelectModule,
+    MatTableModule,
+    MatDividerModule,
+    NgIf,
+    MatMenuModule,
+    LoginComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(private router: Router, private el: ElementRef) {
+    
+    
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const url = this.router.url;
+        const items = [
+          'dashboard',
+          'produits',
+          'commande',
+          'livraisons',
+          'categorie',
+          'client',
+          'payement',
+          'personnel',
+          'utilisateurs',
+          'stock',
+          'parametres',
+          'test'
+        ]; // Add all your routes here
+
+        items.forEach((item) => {
+          const element = this.el.nativeElement.querySelector(`#${item}Item`);
+          if (element) {
+            element.classList.remove('active');
+            if (url.includes(item)) {
+              element.classList.add('active');
+            }
+          }
+        });
+      }
+    });
+  }
+
   title = 'clinic-management';
+
+  logIN: boolean = false;
+
+  ngOnInit(): void {
+    let auth = localStorage.getItem('authToken');
+    if (auth != null) {
+      this.logIN = true;
+    }
+  }
+
+  verifier(value:any) {    
+    this.logIN = value;
+  }
+  logout() {
+    localStorage.clear();
+    this.logIN = false;
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +10,41 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  @Output() loginSuccess = new EventEmitter<boolean>(); // Émettre un événement de succès de connexion
+
+  constructor(private authService : AuthService, private router: Router) {}
+
+  login() {
+    const credentials = { email: this.email, password: this.password };
+    this.authService.login(credentials).subscribe(
+      (response) => {
+        this.authService.saveToken(response.token);
+        this.loginSuccess.emit(true); // Émettre l'événement de succès
+
+        this.email = ''; // Réinitialiser l'email
+        this.password = ''; // Réinitialiser le mot de passe
+        this.router.navigate(['dashboard']).then(() => {
+          // Utiliser location.reload() après la redirection
+          location.reload(); // Recharger la page
+        });
+        this.resetForm(); // Réinitialiser le formulaire
+      },
+      (error) => {
+        if (error.status === 401) {
+          this.errorMessage = 'Email ou mot de passe incorrect';
+        } else {
+          this.errorMessage = 'Une erreur est survenue, veuillez réessayer.';
+        }
+      }
+    );
+  }
+  resetForm() {
+    throw new Error('Method not implemented.');
+  }
 
 }
